696d017e82f0ed48af6345178db90f2bfb07c5f4
bsanderson <sanderson.be@northeastern.edu>
2023-04-08 16:31:02 -0400

    scrape playoff data

--- main.py
+++ main.py
@@ -5,6 +5,29 @@ Final Project
 import pandas as pd
 
 pd.set_option('display.max_columns', None)
+MIN_YEAR = 2010
+MAX_YEAR = 2023
+
+
+def get_playoffs():
+    """
+
+
+    """
+    # read in the playoff data
+    playoff_url = "https://www.basketball-reference.com/playoffs/series.html"
+    df_playoffs = pd.read_html(playoff_url, header=1)[0]
+
+    # drop years below lowest year we're using
+
+    # drop the columns we don't care about and rename the columns left
+
+    # remove parentheses from team names
+
+    # rename playoff series to round
+
+    print(df_playoffs)
+    return df_playoffs
 
 
 def scrape_years(years):
@@ -22,19 +45,18 @@ def scrape_years(years):
         dictionary containing years referencing data frames
     """
     years_dict = {}
-    URL = "https://www.basketball-reference.com/leagues/NBA_{}.html"
+    season_url = "https://www.basketball-reference.com/leagues/NBA_{}.html"
 
     # loop through all the years in the list
     for year in years:
 
         # adjust the url to each year
-        url = URL.format(year)
+        url = season_url.format(year)
 
         # read in data
         df_p100 = pd.read_html(url, header=0, match='Per 100 Poss Stats')[0]
         df_adv = pd.read_html(url, header=1, match='Advanced Stats')[0]
         df_shoot = pd.read_html(url, header=1, match='Shooting Stats')[0]
-        #df_playoffs = pd.read_html(url, header=None, match='Playoff Series')
 
         # clean per 100 data
         df_p100.drop(['Rk', 'G', 'FG', 'FGA', 'FG%', '3P', '3PA', '3P%', '2P', '2PA', '2P%'],
@@ -48,18 +70,19 @@ def scrape_years(years):
         df_adv.drop(['Rk', 'W', 'L', 'Arena', 'Attend.'], inplace=True, axis=1)
         df_adv.drop([30], inplace=True, axis=0)
         df_adv.dropna(axis=1, inplace=True)
-        for i in range(15, 19):
+        # rename columns in dataset
+        for i in range(14, 18):
             df_adv.columns.values[i] = 'off_' + df_adv.columns.values[i]
-        for i in range(19, 23):
+        for i in range(18, 22):
             df_adv.columns.values[i] = 'def_' + df_adv.columns.values[i][:-2]
         df_adv.sort_values("Team", inplace=True)
         df_adv.reset_index(inplace=True, drop=True)
 
         # clean shooting data
-        # clean shooting data
         df_shoot.drop(['Rk', 'G', 'MP'], inplace=True, axis=1)
         df_shoot.drop([30], inplace=True, axis=0)
         df_shoot.dropna(axis=1, inplace=True)
+        # rename columns in dataset
         for i in range(3, 9):
             df_shoot.columns.values[i] = 'fga_perc_' + df_shoot.columns.values[i]
         for i in range(9, 15):
@@ -78,8 +101,7 @@ def scrape_years(years):
 
         # merge all the dataframes into one larger one with all the stats per team
         cur_df = pd.merge(pd.merge(df_p100, df_adv, on='Team'), df_shoot, on='Team')
-        print(cur_df)
-        #print(df_playoffs)
+        # print(cur_df)
 
     return years_dict
 
@@ -87,10 +109,12 @@ def scrape_years(years):
 def main():
 
     # create the list of years we want data for
-    years = list(range(2010, 2024))
+    years = list(range(MIN_YEAR, MAX_YEAR + 1))
 
-    temp_years = [2023]
-    scrape_years(temp_years)
+    #temp_years = [2022]
+    #scrape_years(temp_years)
+
+    get_playoffs()
 
 
 main()
