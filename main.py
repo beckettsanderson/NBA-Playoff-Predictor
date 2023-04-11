4ac858c4f4d3d828b634939034fc4491701d8ea8
bsanderson <sanderson.be@northeastern.edu>
2023-04-11 16:46:40 -0400

    concat the datasets

--- main.py
+++ main.py
@@ -3,25 +3,20 @@ Final Project
 """
 
 import pandas as pd
-import load_data as ld
 
 pd.set_option('display.max_columns', None)
-MIN_YEAR = 2010
-MAX_YEAR = 2022
+ALL_SEASONS = "seasons_data.csv"
+CUR_SEASON = "2023_data.csv"
 
 
 def main():
 
-    # create the list of years we want data for
-    years = list(range(MIN_YEAR, MAX_YEAR + 1))
+    # read in our csv's of data
+    all_df = pd.read_csv(ALL_SEASONS)
+    df_23 = pd.read_csv(CUR_SEASON)
 
-    # get the playoff data
-    df_playoffs = ld.get_playoffs(MIN_YEAR)
-
-    # load in the entirety of the
-    df = ld.scrape_years(years, df_playoffs)
-    print(df)
-    print(df.describe)
+    # concat the data
+    df = pd.concat([all_df, df_23], ignore_index=True)
 
 
 main()
